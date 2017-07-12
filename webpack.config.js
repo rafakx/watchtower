cb592d531f66f8a65e30adbc76d2617e96ee63b2
Rafael Rubenich <rafakx@localhost.localdomain>
2017-07-12 00:59:24 -0300

    Added PropTypes lib, plugin to use arrow function class methods, loaders for css and files, UI lib and Chart Lib

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './main/index.html',
+  template: './src/index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
 module.exports = {
-	entry: './main/index.js',
+	entry: './src/index.js',
 	output: {
 	  path: path.resolve('dist'),
 		filename: 'bundle.js'
@@ -16,7 +16,10 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.sass/, loaders: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.css/, loaders: ['style-loader', 'css-loader'] },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
