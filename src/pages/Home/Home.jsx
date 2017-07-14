39308ec83276489c17b8634417d5f84df3d7e340
Rafael Rubenich <rfrubenich@gmail.com>
2017-07-13 22:41:38 -0300

    Add new chart and UX tests

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,70 @@
 import React, { Component } from 'react';
 import { Grid, Column, Header, Feed, Icon, } from 'semantic-ui-react';
 import Item from '../../components/Item.jsx';
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, RadialChart} from 'react-vis';
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, RadialChart, VerticalGridLines, VerticalBarSeries} from 'react-vis';
 
 export default class Home extends Component {
 
-  KPI = ({ title }) => (
-    <div>
-      <Header as='h3'>{title}</Header>    
-      <XYPlot
-        width={300}
-        height={200}>
-        <HorizontalGridLines />
-        <LineSeries
-          data={[
-            {x: 1, y: 10},
-            {x: 2, y: 4},
-            {x: 3, y: 1}
-          ]}/>
+  BurnDown = () => (
+    <XYPlot
+      width={300}
+      height={200}>
+      <HorizontalGridLines />
+      <LineSeries
+        data={[
+          {x: 1, y: 10},
+          {x: 2, y: 4},
+          {x: 3, y: 1}
+        ]}/>
 
-        <LineSeries
-          strokeStyle='dashed'
-          data={[
-            {x: 1, y: 10},
-            {x: 2, y: 5},
-            {x: 3, y: 0}
-          ]}/>
-        <XAxis />
-        <YAxis />
-      </XYPlot>
-      <Header as='h3'>{title}</Header>          
+      <LineSeries
+        strokeStyle='dashed'
+        data={[
+          {x: 1, y: 10},
+          {x: 2, y: 5},
+          {x: 3, y: 0}
+        ]}/>
+      <XAxis />
+      <YAxis />
+    </XYPlot>
+  );
+
+  Radial = () => (
+    <div style={{'padding-left': '30px'}}>
       <RadialChart
-        innerRadius={70}
-        radius={100}
+        innerRadius={60}
+        radius={90}
         showLabels
         data={ [{angle: 1, label: 'To do'}, {angle: 2, label: 'Doing'}, {angle: 2, label: 'Done'}]}
         width={240}
-        height={240}  />
+        height={200}  />
     </div>
-  );
+  )
+
+  Bar = () => (
+    <XYPlot
+    margin={{bottom: 70}}
+    xType="ordinal"
+    width={300}
+    height={200}>
+    <VerticalGridLines />
+    <HorizontalGridLines />
+    <XAxis tickLabelAngle={-45} />
+    <YAxis />
+    <VerticalBarSeries
+      data={[
+        {x: 'Apples', y: 10},
+        {x: 'Bananas', y: 5},
+        {x: 'Cranberries', y: 15}
+      ]}/>
+    <VerticalBarSeries
+      data={[
+        {x: 'Apples', y: 12},
+        {x: 'Bananas', y: 2},
+        {x: 'Cranberries', y: 11}
+      ]}/>
+    </XYPlot>
+  )
 
   render() {
     return (
@@ -58,7 +84,15 @@ export default class Home extends Component {
           <Item task="31200" title="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
         </Grid.Column>
         <Grid.Column width={4}>
-          <this.KPI title="Burndown" />
+          <Header as='h3'>Burndown</Header>
+          <this.BurnDown />
+          <Header as='h3'>Tempo total por número de tarefas</Header>    
+          {/*<this.Radial />*/}
+          <this.Bar />
+          <br />
+          <div style={{'text-align': 'right', 'margin-right': '15px'}}>
+          <a>Todos indicadores <Icon name="arrow right" /></a>
+          </div>
         </Grid.Column>
       </Grid>
     );
